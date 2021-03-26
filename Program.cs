6a74d7cb64f26b1be32322579aecdac037b343cd
Markus Malz <markus.malz@outlook.com>
2021-03-26 20:41:31 +0100

    and a comma powered by whatthecommit.com

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fig113a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w, x, y, z;
+            int i = 4; int j = 5;
+             {
+                 int j = 7;
+                 i = 6;
+                 w = i + j;
+             }
+             x = i + j;
+             {
+                 int i = 8;
+                 y = i + j;
+             }
+             z = i + j;
+
+             Console.WriteLine("{0} {1} {2} {3}", w, x, y, z);
+        }
+    }
+}
